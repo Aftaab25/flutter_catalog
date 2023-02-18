c730588b0e88f7edae5e2e30df91dea4e4aba56a
Aftaab Siddiqui <aftaab2507@gmail.com>
2023-02-18 16:37:30 +0530

    day 4 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,16 +3,65 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-            color: Colors.blue,
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
+    return Material(
+      color: Colors.white,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 50,
           ),
-        ),
+          Image.asset(
+            'assets/images/login_image.png',
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 16,
+              horizontal: 32,
+            ),
+            child: Column(
+              children: [
+                TextFormField(
+                  // ignore: prefer_const_constructors
+                  decoration: InputDecoration(
+                    hintText: "Enter username",
+                    labelText: "Username",
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  // ignore: prefer_const_constructors
+                  decoration: InputDecoration(
+                    hintText: "Enter password",
+                    labelText: "Password",
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    print("login Successful!");
+                  },
+                  child: const Text("Login"),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
